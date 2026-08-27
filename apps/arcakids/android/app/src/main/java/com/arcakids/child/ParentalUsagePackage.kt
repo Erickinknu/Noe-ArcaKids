2cc6fd3a2e8753afcdf09983f85b57b9a317e8af
Erickinknu <e.inca2011@gmail.com>
2026-08-26 21:12:33 -0500

    fix: restore native Kotlin files deleted by expo prebuild --clean

---
+++ apps/arcakids/android/app/src/main/java/com/arcakids/child/ParentalUsagePackage.kt
@@ -0,0 +1,18 @@
+package com.arcakids.child
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+class ParentalUsagePackage : ReactPackage {
+  override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> =
+    listOf(ParentalUsageModule(reactContext))
+
+  @Deprecated(
+    "Deprecated in Java",
+    ReplaceWith("createViewManagers(reactContext)")
+  )
+  override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> =
+    emptyList()
+}
